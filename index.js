958396ca58d221b7d1a67e988ba56ea5bf6b8104
qasimraheem <qasimmehmood13936@gmail.com>
2023-05-19 15:07:36 +0500

    libp2p app

--- index.js
+++ index.js
@@ -24,6 +24,29 @@ const main = async () => {
         console.log(addr.toString())
     })
 
+
+    // ping peer if received multiaddr
+    if (process.argv.length >= 3) {
+        const ma = multiaddr(process.argv[2])
+        console.log(`pinging remote peer at ${process.argv[2]}`)
+        const latency = await node.ping(ma)
+        console.log(`pinged ${process.argv[2]} in ${latency}ms`)
+    } else {
+        console.log('no remote peer address given, skipping ping')
+    }
+
+
+    const stop = async () => {
+        // stop libp2p
+        await node.stop()
+        console.log('libp2p has stopped')
+        process.exit(0)
+    }
+
+    process.on('SIGTERM', stop)
+    process.on('SIGINT', stop)
+
+
     // stop libp2p
     // await node.stop()
     // console.log('libp2p has stopped')
